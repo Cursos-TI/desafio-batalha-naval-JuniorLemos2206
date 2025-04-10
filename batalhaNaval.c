a33f7c77fafa471d7d3a934caf6856d9c2d0f7cc
JuniorLemos2206 <luiz271193@hotmail.com>
2025-04-10 03:26:27 +0000

    Desafio do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,44 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+   
+       int target = 2;
+       int found = 0;
+   
+   
+    // AGUA = 0; NAVIO CONE = 1; NAVIO CRUZ = 2; NAVIO OCTAEDRO = 3;
+   
+   
+      int matriz[10][10] = { {0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 
+                             {1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, 
+                             {1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 2, 0, 0, 0, 0, 0, 0, 0},
+                             {2, 2, 2, 2, 2, 0, 0, 0, 0, 0}, 
+                             {0, 0, 2, 0, 0, 0, 3, 0, 0, 0}, 
+                             {0, 0, 0, 0, 0, 3, 3, 3, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 3, 0, 0, 0}
+                             };
+   
+   
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+           if (matriz[i][j] == target) {
+               printf("Area %d afetada na posição (%d, %d)\n", target, i, j);
+               found = 1;
+               break;
+           }
+       }
+       if (found) break;
+   }
+   
+   if (!found) {
+       printf("área  %d não afeada no tabuleiro\n", target);
+   }
+   
+    
+    }
+   
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
-}
